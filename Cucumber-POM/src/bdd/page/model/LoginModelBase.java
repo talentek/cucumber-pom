0885d01f478462e340c4e18958ec0ce6d3c966d8
Mohammad N Haq <myemail@myemail.com>
2017-06-04 19:52:15 -0400

    LoginModelBase class created

--- Cucumber-POM/src/bdd/page/model/LoginModelBase.java
+++ Cucumber-POM/src/bdd/page/model/LoginModelBase.java
@@ -1,14 +1,37 @@
 package bdd.page.model;
 
-
-
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-public class LoginModelBase {
-	WebDriver driver;
+public class LoginModelBase extends BaseClass {
+
+	WebElement login = null;
+	WebElement username = null;
+	WebElement password = null;
+	WebElement subbtn = null;
 
 	public LoginModelBase(WebDriver driver) {
-		this.driver = driver;
+		super(driver);
+	}
+
+	public WebElement login() {
+		login = driver.findElement(By.xpath(".//*[@id='ups-navItems']/ul/li[1]/a"));
+		return login;
+	}
+
+	public WebElement userName() {
+		username = driver.findElement(By.xpath(".//*[@id='email']"));
+		return username;
+	}
+
+	public WebElement passWord() {
+		password = driver.findElement(By.xpath(".//*[@id='pwd']"));
+		return password;
+	}
+
+	public WebElement submit() {
+		subbtn = driver.findElement(By.xpath(".//*[@id='submitBtn']"));
+		return subbtn;
 	}
 }
